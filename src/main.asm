cc87cd2ec1b150961f5bbfa35389c37d78337058
Maaz Ahmed <mabdulahmed02@gmail.com>
2024-09-15 17:52:20 +0100

    Create basic operating system

--- src/main.asm
+++ src/main.asm
@@ -1,1 +1,14 @@
-; Main file
+org 0x7C00	; Set code origin to 0x7C00, where BIOS loads the bootloader
+bits 16	; Set processor to 16-bit mode (real mode)
+
+
+; Entry point of the program
+main:
+	hlt	; Halt CPU execution
+	
+.halt:
+	jmp .halt	; Infinite loop to prevent further execution
+	
+; Ensures the boot sector is exactly 512 bytes long
+times 510-($-$$) db 0	; Fill the rest of the boot sector with zeros (up to 510 bytes)
+dw 0AA55h	; Boot signature (0xAA55), required for valid boot sector
